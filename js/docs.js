ff85dfa2d78fd3014bde28333b57f61518e8a02c
Jonas <60574217+Jodekq@users.noreply.github.com>
2024-01-27 15:23:26 +0100

    Changes

---
+++ js/docs.js
@@ -0,0 +1,6 @@
+function scrollToSection(sectionId) {
+    let section = document.getElementById(sectionId);
+    if (section) {
+      section.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
+  }
